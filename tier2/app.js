250459111afc27a53eb6ab47ca7208bd4dcc9816
Juan Lopez <juanlr786@gmail.com>
2023-02-01 09:31:26 -0500

    redirect fix
    makes the new files show up immediately after uploading

--- tier2/app.js
+++ tier2/app.js
@@ -92,13 +92,20 @@ app.post("/upload", (req, res) => {
 
     let options = {
         host: '10.128.0.2', port: port, path: '/upload', method: 'POST', headers: {
-            'Content-Type': 'application/json', 'Content-Length': Buffer.byteLength(postData),
+            'Content-Type': 'application/json'
         }
     };
 
-    let httpRequest = http.request(options, function (res) {
-        console.log(`STATUS: ${res.statusCode}`);
-        console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+    let httpRequest = http.request(options, function (response) {
+        console.log(`STATUS: ${response.statusCode}`);
+        response.setEncoding('utf8');
+        response.on('data', (chunk) => {
+            console.log(`BODY: ${chunk}`);
+        });
+        response.on('end', () => {
+            console.log('No more data in response.');
+            res.send('Tier 2 upload complete.');
+        });
     });
 
     httpRequest.write(postData);
